795943cd45338aec95ad70caf9db5f7f2bccaefc
a-george1 <georgealdrin3@gmail.com>
2019-12-03 14:35:24 +0000

    done

--- aldrin/hsm-c2g/script.sh
+++ aldrin/hsm-c2g/script.sh
@@ -1,9 +1,10 @@
 # openssl req -new -newkey rsa:2048 -nodes -x509 -out rootaldrin.crt -keyout csrkey.pem -subj '/CN=subCA/C=GB/ST=Suffolk/L=Ipswich/O=OnlineShop/OU=groceries/' -config config.cnf
 
 
-openssl req -new -newkey rsa:2048 -nodes -out interaldrin.csr -keyout csrkey.pem -subj '/CN=subCA/C=GB/ST=Suffolk/L=Ipswich/O=OnlineShop/OU=groceries/' -config config.cnf
+# openssl req -new -newkey rsa:2048 -nodes -out interaldrin.csr -keyout csrkey.pem -subj '/CN=subCA/C=GB/ST=Suffolk/L=Ipswich/O=OnlineShop/OU=groceries/' -config config.cnf
 
-# openssl req -new -newkey rsa:2048 -nodes -out end.csr -keyout end.key -subj '/CN=endentity/C=GB/ST=Suffolk/L=Ipswich/O=OnlineShope/OU=groceries/'
-#openssl x509 -req -days 365 -in end.csr -out end.crt -CA inter.crt -CAkey csrkey.pem -CAcreateserial -extfile config -extensions end  
+openssl req -new -newkey rsa:2048 -nodes -out end.csr -keyout end.key -subj '/CN=endentity/C=GB/ST=Suffolk/L=Ipswich/O=OnlineShope/OU=groceries/'
+openssl x509 -req -days 365 -in end.csr -out end.crt -CA interaldrin.crt -CAkey interaldrin.key -CAcreateserial -extfile end-config.cnf -extensions end_crt 
 
-# openssl rsa -in end.key -pubout -out endpub.key
+
+  
