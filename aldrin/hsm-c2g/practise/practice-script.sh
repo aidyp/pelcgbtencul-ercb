e130c2c330387769f08db78a0051c33aad74f5f4
a-george1 <georgealdrin3@gmail.com>
2019-12-03 09:51:39 +0000

    c

--- aldrin/hsm-c2g/practise/practice-script.sh
+++
@@ -1,9 +0,0 @@
-module=/usr/local/lib/softhsm/libsofthsm2.so
-
-pkcs11-tool --module $module --login -p 23456 --keypairgen --key-type rsa:4096 --label "rootaldrinkey" --id 1007 --usage-sign --slot 0x79b8e138
-
-openssl req -out rootaldrincert.pem -nodes -key slot_2042159416-label_rootaldrinkey -keyform engine -config engine.conf -x509 -days 3650 -subj '/CN=ShopCA/C=GB/ST=Suffolk/L=Ipswich/O=Shop/OU=OnlineShop/' -sha256 -engine pkcs11
-
-openssl req -new -newkey rsa:2048 -nodes -out interaldrin.csr -keyout interaldrin.pem -subj  '/CN=ShopCA/C=GB/ST=Suffolk/L=Ipswich/O=Shop/OU=OnlineShop/'
-
-# OPENSSL_CONF=engine.conf openssl x509 -req -CAkeyform engine -engine pkcs11 -in interaldrin.pem -CA 
--- aldrin/hsm-c2g/script.sh
+++ aldrin/hsm-c2g/script.sh
@@ -1,2 +1,9 @@
+# openssl req -new -newkey rsa:2048 -nodes -x509 -out rootaldrin.crt -keyout csrkey.pem -subj '/CN=subCA/C=GB/ST=Suffolk/L=Ipswich/O=OnlineShop/OU=groceries/' -config config.cnf
 
-openssl req -new -newkey rsa:2048 -nodes -out shop.csr -keyout csrkey.pem -subj '/CN=subCA/C=GB/ST=Suffolk/L=Ipswich/O=OnlineShop/OU=groceries/'
+
+openssl req -new -newkey rsa:2048 -nodes -out interaldrin.csr -keyout csrkey.pem -subj '/CN=subCA/C=GB/ST=Suffolk/L=Ipswich/O=OnlineShop/OU=groceries/' -config config.cnf
+
+# openssl req -new -newkey rsa:2048 -nodes -out end.csr -keyout end.key -subj '/CN=endentity/C=GB/ST=Suffolk/L=Ipswich/O=OnlineShope/OU=groceries/'
+#openssl x509 -req -days 365 -in end.csr -out end.crt -CA inter.crt -CAkey csrkey.pem -CAcreateserial -extfile config -extensions end  
+
+# openssl rsa -in end.key -pubout -out endpub.key
