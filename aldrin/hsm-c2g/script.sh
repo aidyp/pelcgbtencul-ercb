9870a72ec25663112ed14a3aeee27282f99d45f8
georgealdrin3@gmail.com <georgealdrin3@gmail.com>
2020-02-04 17:13:01 +0000

    tidy

--- aldrin/hsm-c2g/script.sh
+++ aldrin/hsm-c2g/script.sh
@@ -1,7 +1,16 @@
+#!/bin/bash
+
+###############################ROOT##############
+
 # openssl req -new -newkey rsa:2048 -nodes -x509 -out rootaldrin.crt -keyout csrkey.pem -subj '/CN=subCA/C=GB/ST=Suffolk/L=Ipswich/O=OnlineShop/OU=groceries/' -config config.cnf
 
 
+############INTERMEDIATE###############
+
+
 # openssl req -new -newkey rsa:2048 -nodes -out interaldrin.csr -keyout csrkey.pem -subj '/CN=subCA/C=GB/ST=Suffolk/L=Ipswich/O=OnlineShop/OU=groceries/' -config config.cnf
+
+#######################ENDENTITY#####################
 
 openssl req -new -newkey rsa:2048 -nodes -out end.csr -keyout end.key -subj '/CN=localhost/C=GB/ST=Suffolk/L=Ipswich/O=OnlineShope/OU=groceries/'
 openssl x509 -req -days 365 -in end.csr -out end.crt -CA interaldrin.crt -CAkey interaldrin.key -CAcreateserial -extfile end-config.cnf -extensions end_crt 
