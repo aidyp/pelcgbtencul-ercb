0766c42c430f9569bfabaa337348e7cd3d039e96
aidyp <aidan.j.patrick@gmail.com>
2020-09-22 20:53:21 +0100

    cleaned things up a bit

--- rsa/signing-server.py
+++ rsa/signing-server.py
@@ -1,5 +1,4 @@
-import socket
-import json
+from connector import netcat, to_json
 
 host = "socket.cryptohack.org"
 port = 13374
@@ -15,35 +14,6 @@ c = m^e (N)
 Then "sign" the message
 sign(c) = c^d = m^e^d = m^ed = m (N)          | By definition of RSA
 '''
-
-def netcat(host, port, content):
-	'''
-	Code to send/receive data from the TCP socket
-	'''
-	ret = []
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect((host, int(port)))
-	s.sendall(content.encode())
-	s.shutdown(socket.SHUT_WR)
-	while True:
-		data = s.recv(4096)
-		if not data:
-			break
-		ret.append(data)
-	s.close()
-	return ret
-
-def to_json(data):
-	'''
-	Add some error checking 
-	'''
-	try:
-		temp = data[1].decode('utf-8')
-		temp = temp.split('\n')
-		return json.loads(temp[0])
-	except IndexError:
-		return -1
-
 
 def get_pubkey():
 	'''
