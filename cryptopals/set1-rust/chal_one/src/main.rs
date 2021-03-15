729454011aca50fc1e44749f620f77367c31a0a0
Patrick <pataidan@08f8bc67983f.ant.amazon.com>
2021-03-15 17:08:58 +0000

    finished challenge 2

--- cryptopals/set1-rust/chal_one/src/main.rs
+++ cryptopals/set1-rust/chal_one/src/main.rs
@@ -5,11 +5,21 @@ use std::fs;
 extern crate hex;
 extern crate base64;
 
+fn from_hex(hex_str: &str) -> Vec<u8> {
+    let bytes = hex::decode(hex_str).expect("Decoding failed");
+    return bytes;
+}
+
+fn to_hex(bytes: Vec<u8>) -> String {
+    let hex_str = hex::encode(bytes);
+    return hex_str;
+}
+
 fn challenge_one(filename: &String) {
     let contents = fs::read_to_string(filename).expect("Something went wrong!");
 
     /* get raw bytes */
-    let bytes = hex::decode(contents).expect("Decoding failed");
+    let bytes = from_hex(&contents);
 
     /* convert to base64 */
     let b64 = base64::encode(&bytes);
@@ -22,8 +32,24 @@ fn challenge_two(filename: &String) {
     let contents = fs::read_to_string(filename).expect("Something went wrong!");
 
     /* File contains two hex strings that must be XOR'd together */
-
+    let lines: Vec<&str> = contents.split("\n").collect();
     
+    let (left, right) = (from_hex(&lines[0]), from_hex(&lines[1]));
+    let res = vec_byte_xor(left, right);
+    println!("result: {}", to_hex(res));
+
+
+}
+
+fn vec_byte_xor(v1: Vec<u8>, v2: Vec<u8>) -> Vec<u8> {
+    /* using iterators */
+    let v3: Vec<u8> = v1
+        .iter()
+        .zip(v2.iter())
+        .map(|(&x1, &x2)| x1 ^ x2)
+        .collect();
+    
+    return v3;
 }
 
 fn main() {
@@ -34,6 +60,7 @@ fn main() {
 
     match challenge.as_str() {
         "1" => challenge_one(challenge_file),
+        "2" => challenge_two(challenge_file),
         _ => println!("You haven't written code for this challenge yet!"),
     }
 }
